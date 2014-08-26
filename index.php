2db290ca61d7c8bded25c59ae721234ba885826b
GreyLegion <gray.legion.ysp@gmail.com>
2014-08-26 18:52:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?PHP
 include 'constructComment_tree.php'; ## подключаем файл для использования функцыи outTree(0, 0)
-include 'check_connection_database.php'; ##подключаем файл для использования функцыи checkConnection_database()
+include 'constructComment_tree.php';##'check_connection_database.php'; ##подключаем файл для использования функцыи checkConnection_database()
 $name = $_POST['n'];
 $ucomment = $_POST['ucomment'];
 
 if ($name!=NULL && $ucomment!=NULL){
-	$mysqli = checkConnection_database();## вызов функцыи из файла check_connection_database.php
+	$mysqli1 = Connection_database();## вызов функцыи из файла check_connection_database.php
 	$ins = "INSERT INTO  `comment_table` (`name` , `comment`) VALUES ('$name', '$ucomment')";
-	$w_b = $mysqli->query($ins);
-	$mysqli->close();
+	$w_b = $mysqli1->query($ins);
+	$mysqli1->close();
 	}
 	
 outTree(0, 0); ## вызов функцыи из файла constructComment_tree.php
