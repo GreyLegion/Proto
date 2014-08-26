6f0099642dee6347b173ae5c3dc2db46b16f30f2
GreyLegion <gray.legion.ysp@gmail.com>
2014-08-26 15:49:42 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,36 @@
-#пока все работает
-
-
-
-<form name='data_base' method='post'>
-	  <p><b>Name: </b>
-	  <input name="n" type="text" size="20">
-	  </p>
-	  <p>Data<br>
-      <textarea name='ucomment'></textarea></p>
-	  
-      <br><input type='submit'>
-	  
-	  </form>
 <?PHP
-	 $name = $_POST['n'];
-	 $ucomment = $_POST['ucomment'];
-	 echo "Name ".$name."<br>";
-     echo "Data ".$ucomment."<br>";
-	 if ($name!=NULL && $ucomment!=NULL)
-	 {
-		$mysqli = new mysqli('localhost', 'root', '', 'test_base');
-		if (mysqli_connect_errno()){
-			printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
-			exit;
-		}
-	
-		$ins = "INSERT INTO  `comment_table` (`name` , `comment`) VALUES ('$name', '$ucomment')";
-		echo $ins;
-		$w_b = $mysqli->query($ins);
-		$mysqli->close();
+include 'constructComment_tree.php'; ## подключаем файл для доступа к содержащимся в нем функцыям
+
+$name = $_POST['n'];
+$ucomment = $_POST['ucomment'];
+
+if ($name!=NULL && $ucomment!=NULL){
+	$mysqli = new mysqli('localhost', 'root', '', 'test_base');
+	if (mysqli_connect_errno()){
+		printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
+		exit;
 	}
-	  ?>	   
+	
+	$ins = "INSERT INTO  `comment_table` (`name` , `comment`) VALUES ('$name', '$ucomment')";
+	$w_b = $mysqli->query($ins);
+	$mysqli->close();
+	}
+	
+outTree(0, 0); ## вызов функцыи из файла constructComment_tree.php
+
+echo $name."<br>";
+echo $ucomment."<br>";
+	  ?>	
+	  <?PHP
+
+?>
+<form name='data_base' method='post'>
+<p><b>Name: </b>
+<input name="n" type="text" size="20">
+</p>
+<p>Comment<br>
+<textarea name='ucomment'></textarea></p>
+ 
+<br><input type='submit'>
+
+</form>
