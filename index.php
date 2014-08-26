b0001f4edd6f3bc1d12a438091e0ead0305e8e9c
GreyLegion <gray.legion.ysp@gmail.com>
2014-08-26 16:06:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?PHP
-include 'constructComment_tree.php'; ## подключаем файл для доступа к содержащимся в нем функцыям
-
+include 'constructComment_tree.php'; ## подключаем файл для использования функцыи outTree(0, 0)
+include 'check_connection_database.php'; ##подключаем файл для использования функцыи checkConnection_database()
 $name = $_POST['n'];
 $ucomment = $_POST['ucomment'];
 
 if ($name!=NULL && $ucomment!=NULL){
-	$mysqli = new mysqli('localhost', 'root', '', 'test_base');
-	if (mysqli_connect_errno()){
-		printf("Подключение к серверу MySQL невозможно. Код ошибки: %s\n", mysqli_connect_error());
-		exit;
-	}
-	
+	$mysqli = checkConnection_database();## вызов функцыи из файла check_connection_database.php
 	$ins = "INSERT INTO  `comment_table` (`name` , `comment`) VALUES ('$name', '$ucomment')";
 	$w_b = $mysqli->query($ins);
 	$mysqli->close();
@@ -26,7 +21,7 @@ echo $ucomment."<br>";
 ?>
 <form name='data_base' method='post'>
 <p><b>Name: </b>
-<input name="n" type="text" size="20">
+<input name='n' type="text" size="20">
 </p>
 <p>Comment<br>
 <textarea name='ucomment'></textarea></p>
